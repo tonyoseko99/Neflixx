7ed667e56e4343e2665576f8604fbd3c806e3836
Tonny Oseko <tonnyoseko0@gmail.com>
2023-07-17 06:53:10 +0300

    FEAT: add navbar, footer, and about pages

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-400 py-3">
+      <p className="py-2 text-white text-center sm:py-0">All rights reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
